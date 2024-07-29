079726293e52ce31840ee3ee2d5a818a04d74b58
Trijal Saha <trijalsaha2012@gmail.com>
2024-07-29 17:22:39 -0400

    Delete some unnecessary files.

--- VoodooI2C/VoodooI2C/VoodooI2CController/VoodooI2CTouchscreen.cpp
+++
@@ -1,6 +0,0 @@
-#include "VoodooI2CDeviceProperties.h"
-
-void recognize_gesture(int gesture_type) {
-    // Process the recognized gesture type and perform corresponding actions
-    printf("Recognizing gesture type: %d\n", gesture_type);
-}
--- VoodooI2C/VoodooI2C/VoodooI2CController/VoodooI2CTouchscreenProperties.h
+++
@@ -1,6 +0,0 @@
-#ifndef VoodooI2CTouchscreenProperties_h
-#define VoodooI2CTouchscreenProperties_h
-
-#define TOUCHSCREEN_GESTURE_ENABLED true
-
-#endif /* VoodooI2CTouchscreenProperties_h */
