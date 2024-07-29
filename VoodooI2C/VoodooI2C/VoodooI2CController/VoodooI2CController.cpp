ef32ce17d2259948135fe2613ba5479dfcaa8a0f
Trijal Saha <97483939+Trijal08@users.noreply.github.com>
2024-07-29 15:42:40 -0400

    Update VoodooI2CController.cpp

--- VoodooI2C/VoodooI2C/VoodooI2CController/VoodooI2CController.cpp
+++ VoodooI2C/VoodooI2C/VoodooI2CController/VoodooI2CController.cpp
@@ -5,9 +5,9 @@
 //  Copyright © 2017 Alexandre Daoud. All rights reserved.
 //
 
+#include <iostream>
 #include "VoodooI2CController.hpp"
 #include "VoodooI2CControllerNub.hpp"
-#include <iostream>
 
 // Log only if current thread is interruptible, otherwise we will get a panic.
 #define TryLog(args...) do { if (ml_get_interrupts_enabled()) IOLog(args); } while (0)
