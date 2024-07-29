dbd593b85e9b3226a2f4a794fa077797dc4e2af6
Trijal Saha <97483939+Trijal08@users.noreply.github.com>
2024-07-29 15:31:02 -0400

    Update VoodooI2CController.hpp

--- VoodooI2C/VoodooI2C/VoodooI2CController/VoodooI2CController.hpp
+++ VoodooI2C/VoodooI2C/VoodooI2CController/VoodooI2CController.hpp
@@ -19,7 +19,7 @@
 #define kACPIDevicePathKey "acpi-path"
 #endif
 
-typedef struct {
+struct VoodooI2CControllerPhysicalDevice {
     IOACPIPlatformDevice* acpi_device;
     bool awake = true;
     const char* name;
