61d2e79bc4f08553d9e6e4981fee11e9ffce0da3
Trijal Saha <97483939+Trijal08@users.noreply.github.com>
2024-07-29 16:24:54 -0400

    Recognizes gesture types and triggers corresponding actions, displaying the recognized gesture type.

---
+++ VoodooI2C/VoodooI2C/VoodooI2CController/VoodooI2CTouchscreen.cpp
@@ -0,0 +1,6 @@
+#include "VoodooI2CDeviceProperties.h"
+
+void recognize_gesture(int gesture_type) {
+    // Process the recognized gesture type and perform corresponding actions
+    printf("Recognizing gesture type: %d\n", gesture_type);
+}
